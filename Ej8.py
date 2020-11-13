0d9397609c45ab26aa88ce644e3fff37c8298184
MarcosRolando <marcosrolando.mr@gmail.com>
2020-11-13 00:38:52 -0300

    .

--- Ej8.py
+++ Ej8.py
@@ -1,9 +1,9 @@
 import random as rd
 import math as mt
+import os
+import time as tm
 
-
-#PREGUNTAR BIEN QUE ES LO DE MOVIMIENTO SIN RESTRICCIONES
-#PREGUNTAR SI EL CAMINO ALEATORIO ESTA PREDEFINIDO O SI SE DECIDE EN CADA ESTADO
+#VER BUG DONDE ARRANCAMOS SIN NINGUN CONTAGIADO
 
 class Persona:
 
@@ -21,9 +21,11 @@ class Persona:
         self._estuvo_enfermo = False
         self.turnos_enfermo = 0
         self.se_contagio_este_turno = False
+        self.prob_moverse_derecha = rd.uniform(0.1, 0.9)
+        self.prob_moverse_arriba = rd.uniform(0.1, 0.9)
 
     def puede_contagiar(self):
-        return _esta_enfermo and (not se_contagio_este_turno)
+        return self._esta_enfermo and (not self.se_contagio_este_turno)
 
     #Hace un update del estado de la persona e indica si se debe mover o no
     #Hace el chequeo de si se debe curar
@@ -53,11 +55,11 @@ class Persona:
 
 
 #ver si agregamos parametros para generar poblacion random
-def _generar_region():
+def generar_region():
     region = []
-    for i in range(250):
+    for i in range(30):
         region.append([])
-        for j in range(250):
+        for j in range(30):
             region[i].append(None)
     return region
 
@@ -79,7 +81,7 @@ def _generar_persona_random(cantidades, esta_enferma, se_mueve):
         tipo = "C"
 
     cantidades[tipo] -= 1
-    return Persona(tipo, esta_enferma)
+    return Persona(tipo, esta_enferma, se_mueve)
 
 def _posicion_no_ocupada(region):
     i, j = rd.randint(1, len(region) - 1), rd.randint(1, len(region[0]) - 1)
@@ -88,13 +90,17 @@ def _posicion_no_ocupada(region):
     return i, j
 
 
-def _generar_personas(cantidades, proporcion_contagiados, proporcion_caminantes):
+def _generar_personas(region, cantidades, proporcion_contagiados, proporcion_caminantes):
     lista_personas = []
     contagiados_generados = 0
     caminantes_generados = 0
     cantidad_de_gente = cantidades["A"] + cantidades["B"] + cantidades["C"]
     caminantes_a_generar = int(cantidad_de_gente * proporcion_caminantes)
     contagiados_totales = int(cantidad_de_gente * proporcion_contagiados)
+
+    print("Caminantes a generar: " + str(caminantes_a_generar))
+    print("Contagiados a generar: " + str(contagiados_totales))
+
     probabilidad_caminante = proporcion_caminantes
     probabilidad_contagiado = proporcion_contagiados
     for k in range(cantidad_de_gente):
@@ -132,18 +138,16 @@ def poblar_region(region, cantidad_de_gente, proporcion_caminantes):
     cantidad_B = int(cantidad_de_gente * 0.2)
     cupos_restantes -= (cantidad_A + cantidad_B)
     cantidades = {"A":cantidad_A, "B":cantidad_B, "C":cupos_restantes}
-    return _generar_personas(cantidades, int(cantidad_de_gente * 0.03), proporcion_caminantes)
+    return _generar_personas(region, cantidades, 0.03, proporcion_caminantes)
 
 def posicion_es_valida(region, pos_x, pos_y):
     return (pos_x >= 0) and (pos_x < len(region)) and (pos_y >= 0) \
-            and (pos_y < len(region[0])):
+            and (pos_y < len(region[0]))
 
 
 #Variables globales generales para la configuracion
 probabilidad_de_contagio = 0.65
 hay_recontagio = False
-prob_moverse_derecha = 0.7
-prob_moverse_arriba = 0.6
 
 #funcion para contagiar a la gente cercana al contagiado, recibe el mapa (region) y posicion del contagiado
 def contagiar_cercanos(region, pos_persona_contagiada):
@@ -151,7 +155,10 @@ def contagiar_cercanos(region, pos_persona_contagiada):
     global hay_recontagio
     k = -1
     for i in range(-5, 6):
-        k += 2 if i < 1 else k -= 2
+        if i < 1:
+            k += 2
+        else:
+            k -= 2
         limite_j = mt.floor(k/2)
         for j in range(-limite_j, limite_j + 1):
             pos_x = pos_persona_contagiada[0] + i
@@ -160,76 +167,99 @@ def contagiar_cercanos(region, pos_persona_contagiada):
                 region[pos_x][pos_y].intentar_contagiar(probabilidad_de_contagio, hay_recontagio)
 
 def posicion_esta_libre(region, posicion):
-    return region[posicion[0]][posicion[1]] is not None
+    return region[posicion[0]][posicion[1]] is None
 
-def actualizar_probabilidad_movimiento(region, posicion):
-    global prob_moverse_arriba
-    global prob_moverse_derecha
+def actualizar_probabilidad_movimiento(region, posicion, persona):
     lim_i = len(region) - 1
     lim_j = len(region[0]) - 1
 
-    if (posicion[0] == 0) or (posicion[1] == lim_i):
-        prob_moverse_arriba = 1 - prob_moverse_arriba
+    if (posicion[0] == 0) or (posicion[0] == lim_i):
+        persona.prob_moverse_arriba = 1 - persona.prob_moverse_arriba
 
-    if ( (posicion[0] == 0) and (posicion[1] == 0) ) or ( (posicion[0] == 0) and (posicion[1] == lim_i) )
+    if (posicion[1] == 0) or (posicion[1] == lim_j):
+        persona.prob_moverse_derecha = 1 - persona.prob_moverse_derecha
 
 def mover_persona(region, persona):
-    global prob_moverse_arriba
-    global prob_moverse_derecha
+    anterior_pos = persona[1]
+    logro_moverse = False
+
     if rd.uniform(0, 1) < 0.5: #Se mueve horizontalmente
 
-        if (rd.uniform(0, 1) < prob_moverse_derecha) and (posicion_esta_libre(region, (persona[1][0] + 1, persona[1][1]))):
-            persona[1] = (persona[1][0] + 1, persona[1][1])
-        elif posicion_esta_libre(region, (persona[1][0] - 1, persona[1][1])):
-            persona[1] = (persona[1][0] - 1, persona[1][1])
+        if (rd.uniform(0, 1) < persona[0].prob_moverse_derecha) and (posicion_es_valida(region, anterior_pos[0], anterior_pos[1] + 1)) \
+            and (posicion_esta_libre(region, (anterior_pos[0], anterior_pos[1] + 1))):
+
+            persona[1] = (anterior_pos[0], anterior_pos[1] + 1)
+            logro_moverse = True
+
+        elif (posicion_es_valida(region, anterior_pos[0], anterior_pos[1] - 1)) \
+                and (posicion_esta_libre(region, (anterior_pos[0], anterior_pos[1] - 1))):
+
+            persona[1] = (anterior_pos[0], anterior_pos[1] - 1)
+            logro_moverse = True
 
     else: #Se mueve verticalmente
 
-        if (rd.uniform(0, 1) < prob_moverse_arriba) and (posicion_esta_libre(region, (persona[1][0], persona[1][1] - 1))):
-            persona[1] = (persona[1][0], persona[1][1] - 1)
-        elif posicion_esta_libre(region, (persona[1][0], persona[1][1] + 1)):
-            persona[1] = (persona[1][0], persona[1][1] + 1)
+        if (rd.uniform(0, 1) < persona[0].prob_moverse_arriba) and (posicion_es_valida(region, anterior_pos[0] - 1, anterior_pos[1])) \
+            and (posicion_esta_libre(region, (anterior_pos[0] - 1, anterior_pos[1]))):
 
-    actualizar_probabilidad_movimiento(region, persona[1])
+            persona[1] = (anterior_pos[0] - 1, anterior_pos[1])
+            logro_moverse = True
+
+        elif (posicion_es_valida(region, anterior_pos[0] + 1, anterior_pos[1])) \
+            and (posicion_esta_libre(region, (anterior_pos[0] + 1, anterior_pos[1]))):
+
+            persona[1] = (anterior_pos[0] + 1, anterior_pos[1])
+            logro_moverse = True
+
+    if logro_moverse:
+        region[persona[1][0]][persona[1][1]] = persona[0]
+        region[anterior_pos[0]][anterior_pos[1]] = None #Aca perdi a mi persona para siempre? O no vive en la matriz?
+
+    actualizar_probabilidad_movimiento(region, persona[1], persona[0])
 
 
+def mostrar_region(region):
+    os.system('clear')
 
+    for i in range(len(region)):
+        for j in range(len(region[i])):
 
+            if region[i][j] is not None:
+                if region[i][j].puede_contagiar():
+                    print('C', end = '')
+                else:
+                    print('P', end = '')
+            else:
+                print(' ', end = '')
+
+        print('')
 
 #N: cant de personas
 #alfa: instantes de tiempo minimos para que una persona enferma empiece a tener chance de sanar en cada siguiente instante de tiempo
 #beta: probabilidad de sanar
 #T: instante de tiempo en el que la probabilidad de contagio se reduce un 15%
 def main(N, alfa, beta, T):
+    proporcion_caminantes = 1
     region = generar_region() #Genero el array
-    cantidad_de_gente = 100
-    personas = poblar_region(region, cantidad_de_gente) #Me mete en la region las personitas generadas
+    personas = poblar_region(region, N, proporcion_caminantes) #Me mete en la region las personitas generadas
+    instantes_de_tiempo = 5000
     #Las personas es una lista donde cada elemento es una lista con una persona y una tupla con las coordenadas
-    for persona in personas:
 
-        if persona[0].puede_contagiar():
-            contagiar_cercanos(region, persona[1])
+    for i in range(instantes_de_tiempo):
 
-        if persona[0].intentar_moverse(alfa, beta):
-            mover_persona(region, persona)
+        mostrar_region(region)
+
+        for persona in personas:
+
+            if persona[0].puede_contagiar():
+                contagiar_cercanos(region, persona[1])
+
+            if persona[0].intentar_moverse(alfa, beta):
+                mover_persona(region, persona)
+
+        tm.sleep(0.5)
+
+main(50, 5, 0.9, 1500)
 
 
 
-
-
-
-
-
-                    - - - - - - - - - - -
-                    - - - - - + - - - - -
-                    - - - - + + + - - - -
-                    - - - + + + + + - - -
-                    - - + + + + + + + - -
-                    - + + + + + + + + + -
-                    + + + + + / + + + + +
-                    - + + + + + + + + + -
-                    - - + + + + + + + - -
-                    - - - + + + + + - - -
-                    - - - - + + + - - - -
-                    - - - - - + - - - - -
-                    - - - - - - - - - - -
